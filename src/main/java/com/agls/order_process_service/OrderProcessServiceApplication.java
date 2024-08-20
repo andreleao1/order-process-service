ddc422b9163115ffafa150bd0770f39a01202ba5
andreleao1 <andre.gustavo.ls@hotmail.com>
2024-08-20 17:31:05 -0300

    first commit

---
+++ src/main/java/com/agls/order_process_service/OrderProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agls.order_process_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agls/order_process_service/OrderProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agls.order_process_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
